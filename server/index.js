c379cd4361d06dc5666f8056db46b615dfa30c46
Olk <oikangal@gmail.com>
2025-09-26 22:34:04 +0300

    Pretty JSON output

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('json spaces', 2);
 app.use(cors());
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
